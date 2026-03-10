4dbd4de9e5b71628f15a70d48af87ff50d9a939a
maksim300882 <zxcasdqwe300882@gmail.com>
2026-03-10 17:50:03 +0300

    Углубление в Playwright. Запуск JavaScript-кода на странице

---
+++ playwright_js.py
@@ -0,0 +1,22 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto(
+        "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login",
+        wait_until='networkidle'
+    )
+
+    page.evaluate(
+        """
+        (text) => { // Принимаем аргумент в JS функции
+            const title = document.getElementById('authentication-ui-course-title-text');
+            title.textContent = text;
+        }
+        """,
+        'New Text'
+    )
+
+    page.wait_for_timeout(5000)
