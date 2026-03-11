3ddd41ba43ce10d1e4c8797db248d7848397dfad
maksim300882 <zxcasdqwe300882@gmail.com>
2026-03-11 12:50:16 +0300

    Работа с авторизацией в Playwright. Практика работы с авторизацией в Playwright

---
+++ playwright_courses.py
@@ -0,0 +1,42 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+
+    registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    registration_email_input.fill('user.name@gmail.com')
+
+    registration_login_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    registration_login_input.fill('username')
+
+    registration_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    registration_password_input.fill('password')
+
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
+
+    context.storage_state(path="storage-state.json")
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="storage-state.json")
+    page = context.new_page()
+
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+
+    courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_title).to_have_text('Courses')
+
+    courses_list_empty_title = page.get_by_test_id('courses-list-empty-view-title-text')
+    expect(courses_list_empty_title).to_have_text('There is no results')
+
+    courses_list_empty_icon = page.get_by_test_id('courses-list-empty-view-icon')
+    expect(courses_list_empty_icon).to_be_visible()
+
+    courses_list_empty_description = page.get_by_test_id('courses-list-empty-view-description-text')
+    expect(courses_list_empty_description).to_have_text('Results from the load test pipeline will be displayed here')
+
