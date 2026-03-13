4159930217d3504ac66e55ce318bff2231615abf
maksim300882 <zxcasdqwe300882@gmail.com>
2026-03-13 12:32:09 +0300

    Маркировки Pytest

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,5 +1,8 @@
+import pytest
 from playwright.sync_api import sync_playwright, expect  # Импорт Playwright для синхронного режима и проверки
 
+@pytest.mark.regression
+@pytest.mark.authorization
 def test_wrong_email_or_password_authorization():
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(headless=False)
