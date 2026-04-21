4b2dd6231694e9d420a22a639c9c7614ca39ef2a
whyouseless <zxcasdqwe300882@gmail.com>
2026-04-22 01:29:34 +0300

    Практикуемся в реализации PageObject

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,39 +1,44 @@
 import pytest
-from playwright.sync_api import Page, Playwright
+from playwright.sync_api import Page, Playwright, expect
 
 
-@pytest.fixture()
+@pytest.fixture
 def chromium_page(playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     yield browser.new_page()
     browser.close()
 
 
-@pytest.fixture(scope='session')
-def initialize_browser_state(playwright: Playwright) -> None:
+@pytest.fixture(scope="session")
+def initialize_browser_state(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
 
-    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-    registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    registration_email_input.fill('user.name@gmail.com')
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill('user.name@gmail.com')
 
-    registration_login_input = page.get_by_test_id('registration-form-username-input').locator('input')
-    registration_login_input.fill('username')
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill('username')
 
-    registration_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-    registration_password_input.fill('password')
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill('password')
 
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
 
+    dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
+    expect(dashboard_title).to_have_text('Dashboard')
+
     context.storage_state(path="browser-state.json")
+    browser.close()
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture
 def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state="browser-state.json")
     yield context.new_page()
+    browser.close()
